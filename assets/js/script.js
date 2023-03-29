a29d8fd2d3ce29329aa2519930c054b68e97d999
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-03-30 01:38:45 +0700

    udpate header hero

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".menu__link");
+    const menu = document.querySelector(".menu");
+    const lineEffect = document.createElement("div");
+    // lineEffect.className = ""
+});
